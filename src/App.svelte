7c3195dda6910a7f709a4e4a09d4c8e07301d63e
Jacky Hu <flameddd@gmail.com>
2020-07-18 11:40:55 +0800

    chore: add WorkBox networkTimeoutSeconds config

--- src/App.svelte
+++ src/App.svelte
@@ -170,7 +170,7 @@
       border-top-left-radius: 20px;
       border-top-right-radius: 20px;
       position: absolute;
-      height: 90px;
+      height: 80px;
       bottom: 0;
       left: 0;
       right: 0;
--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,20 @@
 /* eslint-env node */
 export const workboxConfig = {
   globDirectory: 'public/',
-  globPatterns: ['**/*.{png,css,js,html,json}'],
+  globPatterns: ['**/*.{jpg,png,css,js,html,json}'],
   swDest: 'public/sw.js',
   sourcemap: false,
   inlineWorkboxRuntime: true,
   cleanupOutdatedCaches: true,
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|html|css|js|json)$/,
+      handler: 'NetworkFirst',
+      options: {
+        networkTimeoutSeconds: 3,
+      },
+    },
+  ],
 };
 
 export default workboxConfig;
